0084683fb05a0c2a7f7dc0ed83f9db68b222619f
MsEmily1020 <peace10200khs@gmail.com>
2023-05-27 17:35:04 +0900

    [Feat] 숫자 버튼을 클릭했을 때 TextView 입력되기
    Main Activity에 작성
    - 숫자 버튼들에게 id 부여하기
    - id값으로 숫자 분류하기
    - 해당 버튼을 클릭했을 때 해당 숫자 띄우기
    - 결과창의 text 변경을 위해 결과창 TextView 또한, id값 가져오기
    
    Ref: #3

--- app/src/main/java/com/example/calcproject/MainActivity.kt
+++ app/src/main/java/com/example/calcproject/MainActivity.kt
@@ -1,14 +1,37 @@
 package com.example.calcproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var result: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide() // My Application Bar 숨김
+        result = findViewById<TextView>(R.id.text)
+        title = "계산기"
 
+    }
+
+    // 클릭한 버튼 id로 수행
+    fun onClick(view: View) {
+        val id = view.resources.getResourceEntryName(view.id)
+        for (i in 0..9) {
+            if(id == "_${i}") {
+                appendNumber(true, i.toString())
+            }
+        }
+    }
+
+    private fun appendNumber(isNumber: Boolean, value: String) {
+        if (isNumber) {
+            if (result.text == "0") result.text = value
+            else result.append(value)
+        }
     }
 }
