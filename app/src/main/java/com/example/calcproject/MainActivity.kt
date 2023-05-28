1423bccabbb5a25d8cf68136e5f85b478172f906
MsEmily1020 <peace10200khs@gmail.com>
2023-05-28 12:05:29 +0900

    [Feat] 사칙 연산 버튼을 클릭했을 때 TextView 입력
    Main Activity에 작성
    - 사칙 연산 버튼 id 부여하기
    - id값으로 사칙 기호 분류
    - 해당 버튼을 클릭했을 때 기호 띄우기
    - 괄호 && 초기 상태이면 초기값 0을 지우고 괄호 써주기
    - 초기 상태 일 때는 기본적으로 연산 불가능 -> Toast 띄우기
    
    Ref: #4

--- app/src/main/java/com/example/calcproject/MainActivity.kt
+++ app/src/main/java/com/example/calcproject/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -13,24 +14,52 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide() // My Application Bar 숨김
-        result = findViewById<TextView>(R.id.text)
         title = "계산기"
+        result = findViewById<TextView>(R.id.text)
 
     }
 
     // 클릭한 버튼 id로 수행
     fun onClick(view: View) {
         val id = view.resources.getResourceEntryName(view.id)
-        for (i in 0..9) {
-            if(id == "_${i}") {
-                appendNumber(true, i.toString())
+
+        when(id) {
+            "plus" -> appendResult(false, "+")          // 덧셈
+            "subtract" -> appendResult(false, "-")      // 뺄셈
+            "X" -> appendResult(false, "X")             // 곱셈
+            "division" -> appendResult(false, "/")      // 나누기(몫)
+            "remain" -> appendResult(false, "%")        // 나누기(나머지)
+            "bracket1" -> appendResult(false, "(")      // 괄호 열기
+            "bracket2" -> appendResult(false, ")")      // 괄호 닫기
+            "_." -> appendResult(false, ".")            // 소수점
+            
+            // 숫자 버튼
+            else -> {
+                for (i in 0..9) {
+                    if (id == "_${i}") {
+                        appendResult(true, i.toString())
+                    }
+                }
             }
         }
     }
 
-    private fun appendNumber(isNumber: Boolean, value: String) {
+    private fun appendResult(isNumber: Boolean, value: String) {
         if (isNumber) {
             if (result.text == "0") result.text = value
+            else result.append(value)
+        }
+
+        else {
+            if(value == "(" || value == ")") {
+                if (result.text == "0") result.text = value
+                else result.append(value)
+            }
+
+            else if(result.text == "0") {
+                Toast.makeText(this, "완성 되지 않은 수식", Toast.LENGTH_SHORT).show()
+            }
+
             else result.append(value)
         }
     }
